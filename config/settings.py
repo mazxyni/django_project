ded361640718642c20d47849d3d3f0c2b7a8dc94
2005931 <ajinrhdwn@gmail.com>
2021-12-18 22:59:53 +0900

    add static_root

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+sxnq=!+z55(mmm#%c$j-ymt&dcwzsdzue)1-7#xh-x48sg+k1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
